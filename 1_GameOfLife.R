153ebdc298dd869c7ff590ef4685d8d16ec37ffa
civilstat <jerzywieczorek@gmail.com>
2015-01-08 10:59:24 -0500

    Set up initial structure of main program

---
+++ 1_GameOfLife.R
@@ -0,0 +1,36 @@
+#####
+# Jerzy Wieczorek
+# 36-750 Statistical Computing
+# HW1: Game of Life
+# 1/8/2015
+#
+# For program input, create a text file
+# whose 1st line is a single positive integer
+# (n, the number of generations to advance)
+# and whose remaining lines are a binary matrix
+# with 1s at live cells and 0s at dead cells.
+# For example:
+# 
+# 3
+# 010
+# 111
+# 010
+# 
+# Run the program from the command line like so:
+# Rscript 1_GameOfLife.R myInputFile.txt
+# 
+# Output will be written to 1_GameOfLife_Out.txt
+#####
+
+# Read in command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# Check command line arguments:
+# should be just one, the name of the input text file
+
+# Read in and check the contents of the text file
+
+# Loop over generations:
+# Count neighbors and update status
+
+# Write output to a text file
